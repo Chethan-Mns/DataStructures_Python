a0d9e7326b78e1e8b9800f91687a8ed8c4e3659e
Chethan Mungara <mnschethan@gmail.com>
2023-09-03 20:14:34 +0530

    Add files via upload

---
+++ Sorting/Selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(lst):
+    for i in range(len(lst)):
+        min_index = i
+        for j in range(i + 1, len(lst)):
+            if lst[j] < lst[min_index]:
+                min_index = j
+        if min_index != i:
+            lst[i], lst[min_index] = lst[min_index], lst[i]
+    return lst
+
+
+print(selection_sort(list(map(int, input().split()))))
