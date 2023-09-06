a875cf48edbfb5b6531be1968c8803f5f37efaac
Chethan Mungara <mnschethan@gmail.com>
2023-09-06 09:27:59 +0530

    Add files via upload

---
+++ BST/BFS_traversal.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while (temp):
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+  
+    def BFS(self):
+        curernt_node = self.root
+        queue, result = [], []
+        queue.append(curernt_node)
+        while len(queue) > 0:
+            curernt_node = queue.pop(0)
+            result.append(curernt_node.value)
+            if curernt_node.left:
+                queue.append(curernt_node.left)
+            if curernt_node.right:
+                queue.append(curernt_node.right)
+            
+        return result
+
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.BFS())
+
+
+
+
+
+
+
+                
+
+
+
+ 
